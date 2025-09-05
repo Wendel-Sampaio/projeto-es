07e18a40f41e50a80b592b7338c013ecbac67303
NatanCesar <cesardasilvanataniel@gmail.com>
2025-09-05 10:29:17 -0300

    fix bug and add system out

--- HelloWorldRaphael.java
+++ HelloWorldRaphael.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class HelloWorldRaphael {
     public static void main(String[] args) {
         System.out.println("Hello Raphael");
+        System.out.println("Opa!");
     }
 }
